4259d2d4d8392800af1e54bb081e8d43a1881b76
ctemescw <ctemescw@penguin-village.net>
2014-09-23 17:43:51 +0200

    changed the configuration to generate the documentation

--- docs/es/source/conf.py
+++ docs/es/source/conf.py
@@ -182,78 +182,78 @@ htmlhelp_basename = 'meikiandoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-  ('index', 'meikian.tex', u'Documentación de Meikian Live',
-   u'The Meikian Project', 'manual'),
-]
-
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
-
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
-
-# If false, no module index is generated.
-#latex_domain_indices = True
+#latex_elements = {
+## The paper size ('letterpaper' or 'a4paper').
+##'papersize': 'letterpaper',
+#
+## The font size ('10pt', '11pt' or '12pt').
+##'pointsize': '10pt',
+#
+## Additional stuff for the LaTeX preamble.
+##'preamble': '',
+#}
+#
+## Grouping the document tree into LaTeX files. List of tuples
+## (source start file, target name, title,
+##  author, documentclass [howto, manual, or own class]).
+#latex_documents = [
+#  ('index', 'meikian.tex', u'Documentación de Meikian Live',
+#   u'The Meikian Project', 'manual'),
+#]
+#
+## The name of an image file (relative to this directory) to place at the top of
+## the title page.
+##latex_logo = None
+#
+## For "manual" documents, if this is true, then toplevel headings are parts,
+## not chapters.
+##latex_use_parts = False
+#
+## If true, show page references after internal links.
+##latex_show_pagerefs = False
+#
+## If true, show URL addresses after external links.
+##latex_show_urls = False
+#
+## Documents to append as an appendix to all manuals.
+##latex_appendices = []
+#
+## If false, no module index is generated.
+##latex_domain_indices = True
 
 
 # -- Options for manual page output ---------------------------------------
 
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'Meikian Live', u'Documentación de Meikian Live',
-     [u'The Meikian Project'], 1)
-]
-
-# If true, show URL addresses after external links.
-#man_show_urls = False
+## One entry per manual page. List of tuples
+## (source start file, name, description, authors, manual section).
+#man_pages = [
+#    ('index', 'Meikian Live', u'Documentación de Meikian Live',
+#     [u'The Meikian Project'], 1)
+#]
+#
+## If true, show URL addresses after external links.
+##man_show_urls = False
 
 
 # -- Options for Texinfo output -------------------------------------------
 
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-  ('index', 'meikian', u'Documentación de Meikian Live',
-   u'The Meikian Project', 'meikian', 'Distribución basada en Debian GNU/Linux.',
-   'Miscellaneous'),
-]
-
-# Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
-
-# If false, no module index is generated.
-#texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
-
-# If true, do not generate a @detailmenu in the "Top" node's menu.
-#texinfo_no_detailmenu = False
+## Grouping the document tree into Texinfo files. List of tuples
+## (source start file, target name, title, author,
+##  dir menu entry, description, category)
+#texinfo_documents = [
+#  ('index', 'meikian', u'Documentación de Meikian Live',
+#   u'The Meikian Project', 'meikian', 'Distribución basada en Debian GNU/Linux.',
+#   'Miscellaneous'),
+#]
+#
+## Documents to append as an appendix to all manuals.
+##texinfo_appendices = []
+#
+## If false, no module index is generated.
+##texinfo_domain_indices = True
+#
+## How to display URL addresses: 'footnote', 'no', or 'inline'.
+##texinfo_show_urls = 'footnote'
+#
+## If true, do not generate a @detailmenu in the "Top" node's menu.
+##texinfo_no_detailmenu = False
